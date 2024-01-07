18a2de3960f832101940a0d9655bf3d90c9f5a00
sa0720 <155711785+sa0720@users.noreply.github.com>
2024-01-07 11:06:03 +0800

    Update index.vue

--- src/components/lastname/index.vue
+++ src/components/lastname/index.vue
@@ -87,7 +87,7 @@ export default {
             type: 'value',
             name: '进士人数',
             min: 0,
-            max: 80,
+            max: 300,
             position: 'left',
             axisLine: {
               show: true
